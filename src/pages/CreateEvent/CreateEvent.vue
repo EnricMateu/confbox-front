2cc4bc6af73bcadec4c0f797c3ba806df9effb78
EnricMateu <xaxeu@hotmail.com>
2019-11-14 16:52:34 +0100

    button submit added

--- src/pages/CreateEvent/CreateEvent.vue
+++ src/pages/CreateEvent/CreateEvent.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
-    <h2>Create Event</h2>
+    <h2>Event form</h2>
     <span>Event description</span>
     <br />
-    <input type="text" v-model="description" {{description}} />
+    <textarea v-model="Event.description"></textarea>
+    <br />
     <span>From date</span>
+    <br />
+    <input v-model="Event.date_from"/>
     <br />
     <span>To date</span>
     <br />
+    <input v-model="Event.date_to"/>
+    <br />
     <span>City</span>
+    <br />
+    <input v-model="Event.city"/>
     <br />
     <span>Country</span>
     <br />
+    <input v-model="Event.country"/>
+    <br />
     <span>Event URL</span>
     <br />
+    <input v-model="Event.event_url"/>
+    <br />
     <span>Topic</span>
+    <br />
+    <input v-model="Event.topic"/>
+    <br />
+    <button>Submit</button>
   </div>
 </template>
 
