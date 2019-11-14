62e48a51f63ff59a19ab1c673edca8b10553c325
EnricMateu <xaxeu@hotmail.com>
2019-11-14 16:25:40 +0100

    first steps in form creation

--- src/pages/CreateEvent/CreateEvent.vue
+++ src/pages/CreateEvent/CreateEvent.vue
@@ -1,7 +1,49 @@
 <template>
   <div>
-    <h2>Create Events</h2>
-<input v-model="name"
-
+    <h2>Create Event</h2>
+    <span>Event description</span>
+    <br />
+    <input type="text" v-model="description" {{description}} />
+    <span>From date</span>
+    <br />
+    <span>To date</span>
+    <br />
+    <span>City</span>
+    <br />
+    <span>Country</span>
+    <br />
+    <span>Event URL</span>
+    <br />
+    <span>Topic</span>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'CreateEvent',
+  data() {
+    return {
+      Event: {
+        description: '',
+        date_from: '',
+        date_to: '',
+        city: '',
+        country: '',
+        event_url: '',
+        topic: '',
+      },
+    };
+  },
+  created() {
+    // this.fetchEventData();
+  },
+
+  methods: {
+    // async fetchEventData() {
+    //   this.data = await fetch('http://localhost:8081/createEvent').then(
+    //     response => response.json(),
+    //   );
+    // },
+  },
+};
+</script>
